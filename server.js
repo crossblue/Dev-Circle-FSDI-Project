4df0eac3c81e5ca83e81fdc0336832fb531c6073
Kenneth Judd <judd_kenneth@hotmail.com>
2020-07-16 17:38:21 -0600

    Second submission

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 
 const app = express();
 
+
+
+
+//Database Connection
+connectDB();
+
+// Init Middleware
+
+app.use(express.json({
+    extended: false
+}));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+
+// Define Routes
+
+app.use('/api/users', require('./config/routes/api/users'));
+app.use('/api/auth', require('./config/routes/api/auth'));
+app.use('/api/profile', require('./config/routes/api/profile'));
+app.use('/api/post', require('./config/routes/api/post'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Server started on port' + '${PORT}'));
+app.listen(PORT, () => console.log('Server started on port ' + PORT));
