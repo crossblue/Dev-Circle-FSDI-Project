d3f7542ec9253591c6bab6682fca811bdccd9b63
Kenneth Judd <judd_kenneth@hotmail.com>
2020-07-25 22:57:57 -0600

    Setup Redux and Alerts

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
-
 const app = express();
-
-
-
 
 //Database Connection
 connectDB();
 
 // Init Middleware
 
-app.use(express.json({
-    extended: false
-}));
+app.use(
+  express.json({
+    extended: false,
+  })
+);
 
 app.get('/', (req, res) => res.send('API Running'));
-
 
 // Define Routes
 
